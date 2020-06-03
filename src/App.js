49650386758f23923a53e7da82eaf2dfe98f56f6
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-06-03 15:33:18 -0600

    added click event to the h2 tag as example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const App = () => {
-  return(
-    <div>This is a react component</div>
+  const title = "Hello world this is dynamic";
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2 onClick={() => alert("you clicked the message")}>
+        React project, contact list, coming soon!</h2>
+    </div>
   )
 }
 
