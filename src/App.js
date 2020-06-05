062b4aa5d6cc0d7f17a0568f02661d4696f11ce2
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-06-05 11:18:27 -0600

    added contact data to an array, and used props to make the app cleaner and more dynamic

--- src/App.js
+++ src/App.js
@@ -3,23 +3,45 @@ import './App.css';
 import ContactCard from './ContactCard';
 
 const App = () => {
+  const contacts = [
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Jenny Han",
+      email: "jenny.han@fake.com",
+      age: 25,
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Paul Jones", 
+      email: "paul.jones@fake.com",
+      age: 35,
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Peter Pan",
+      email: "peter.pan@fake.com",
+      age: 100,
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Lisa Simpson",
+      email: "lisa.simpson@fake.com",
+      age: 11,
+    },
+  ];
+
   return (
     <div>
-      <ContactCard 
-        avatarUrl="https://via.placeholder.com/150"
-        name="Jenny Han" 
-        email="jenny.han@fake.com"
-        age = {25} />
-      <ContactCard 
-        avatarUrl="https://via.placeholder.com/150"
-        name="Paul Jones" 
-        email="paul.jones@fake.com"
-        age = {35} />
-      <ContactCard 
-        avatarUrl="https://via.placeholder.com/150"
-        name="Peter Pan" 
-        email="peter.pan@fake.com"
-        age = {44} />
+      {contacts.map((contact, index) => {
+        return (
+          <ContactCard 
+            avatarUrl = {contact.avatarUrl}
+            name = {contact.name} 
+            email = {contact.email}
+            age = {contact.age}
+          />
+        )
+      })}
     </div>
   );
 }
